a43867f85bafd9807aa637cc23d736bc4d9f7489
Emiliano Salazar <salazaremiliano84gmail.com>
2023-03-03 14:57:20 -0300

    ejercicio Sistema Educativo

--- 4.introPOO/ejercicio3-matriz/claseMatriz.ts
+++ 4.introPOO/ejercicio3-matriz/claseMatriz.ts
@@ -2,24 +2,41 @@
 Implementar la clase Matriz. En vez de consultar los valores con los corchetes, 
 se debe hacer (desde afuera) a través de un método → get(x, y) */
 
+import { arch } from "os";
+
 export class Matrix {
   private elementXaxis: number;
   private elementYaxis: number;
-  constructor(elementXaxis: number, elementYaxis: number,) {
+  private arregloMatriz:number[][];
+  constructor(elementXaxis: number, elementYaxis: number,arregloMatriz?:number[][]) {
     this.elementXaxis = elementXaxis;
     this.elementYaxis = elementYaxis;
+    arregloMatriz ? (this.arregloMatriz=arregloMatriz):(this.arregloMatriz=[])
   }
-  showMatrix(): number[][] {
-    const arregloMatriz: number[][]= [];
+  createMatrix(): number[][] {
     for (let i = 0; i < this.elementXaxis; i++) {
       for (let j = 0; j < this.elementYaxis; j++) {
-        arregloMatriz[i][j] = Math.floor(Math.random() * (100 - 1) + 1);
-        console.table(arregloMatriz[i][j]);
+        this.arregloMatriz[i][j] = Math.floor(Math.random() * (100 - 1) + 1);
       }
     }
-    return arregloMatriz;
+    return this.arregloMatriz;
   }
+  /* showMatrix():void{
+    let mostrarMatriz:number[][]=[];
+    for (let i = 0; i < this.elementXaxis; i++) {
+      for (let j = 0; j < this.elementYaxis; j++) {
+        mostrarMatriz=this.arregloMatriz[i][j]
+      }
+      console.log(mostrarMatriz);
+      
+    }
+
+
+  } */
 }
 
 const matrix_ejemplo: Matrix = new Matrix(5, 6);
-matrix_ejemplo.showMatrix();
+matrix_ejemplo.createMatrix();
+console.table(matrix_ejemplo.createMatrix());
+
+//EJERCICIO SIN TERMINAR
